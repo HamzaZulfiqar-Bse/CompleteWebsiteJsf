a78dbd0cb210acd23e522b3360b7e0abc4bee2b3
hamzarajaa <rh408905@gmail.com>
2020-04-20 17:57:46 +0500

    forms including backing bean and managed bean dashboard and database

--- src/beans/managedbeans/MyBean.java
+++ src/beans/managedbeans/MyBean.java
@@ -1,17 +1,23 @@
 package beans.managedbeans;
 
 import beans.backingbeans.User;
+import db.DbConnection;
 
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class MyBean {
 private User user;
+private DbConnection dbConnection;
 
     public MyBean() {
-        user= new User();
+        user = new User();
+        dbConnection = new DbConnection();
     }
-
+ public String registerAccount(){
+     dbConnection.registerUser(user.getName(),user.getEmail(),user.getPassword());
+        return null;
+ }
     public User getUser() {
         return user;
     }
